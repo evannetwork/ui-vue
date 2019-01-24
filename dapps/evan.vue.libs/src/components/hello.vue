2bec34b00b87b1a3be902c82c97b5ec5319f5cc6
Tobias Winkler <tobias.winkler@evan.team>
2019-01-24 12:24:46 +0100

    setup initial library structure

---
+++ dapps/evan.vue.libs/src/components/hello.vue
@@ -0,0 +1,55 @@
+/*
+  Copyright (C) 2018-present evan GmbH.
+
+  This program is free software: you can redistribute it and/or modify it
+  under the terms of the GNU Affero General Public License, version 3,
+  as published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program. If not, see http://www.gnu.org/licenses/ or
+  write to the Free Software Foundation, Inc., 51 Franklin Street,
+  Fifth Floor, Boston, MA, 02110-1301 USA, or download the license from
+  the following URL: https://evan.network/license/
+
+  You can be released from the requirements of the GNU Affero General Public
+  License by purchasing a commercial license.
+  Buying such a license is mandatory as soon as you use this software or parts
+  of it on other blockchains than evan.network.
+
+  For more information, please contact evan GmbH at this address:
+  https://evan.network/license/
+*/
+
+<template>
+  <div>
+    <md-content>Background</md-content>
+    <md-content class="md-primary">Primary</md-content>
+    <md-content class="md-accent">Accent</md-content>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import * as bcc from 'bcc';
+  import { System, core, } from 'dapp-browser';
+    
+  export default {
+    name: 'evan-hello',
+    data: function () {
+      return { }
+    },
+    methods: {
+
+    }
+  };
+</script>
+<style lang="scss" scoped>
+  .hello {
+    background: red;
+  }
+</style>
--- dapps/material.vue/src/index.ts
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue';
-import VueMaterial from 'vue-material';
-
-export default VueMaterial;
