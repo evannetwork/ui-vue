23619abaa05bc0de81810a041068609e6a955a0b
Tobias Winkler <tobias.winkler@evan.team>
2019-04-03 16:33:06 +0200

    Fix empty mail read localStorage array

--- dapps/evancore.vue.libs/src/components/dapp-wrapper/dapp-wrapper.ts
+++ dapps/evancore.vue.libs/src/components/dapp-wrapper/dapp-wrapper.ts
@@ -442,7 +442,7 @@ export default class DAppWrapper  extends mixins(EvanComponent) {
     this.userInfo.mailsLoading = true;
 
     // load mail inbox informations, load 10 for checking for +9 new mails
-    this.userInfo.readMails = JSON.parse(window.localStorage['evan-mail-read'] || [ ]);
+    this.userInfo.readMails = JSON.parse(window.localStorage['evan-mail-read'] || '[ ]');
     this.userInfo.newMailCount = 0;
 
     let mails = [ ];
