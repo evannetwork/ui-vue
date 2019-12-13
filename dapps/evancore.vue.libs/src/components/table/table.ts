4eeda63eba2beca842c3a6fa7af5780379630794
dtrinh <duc.trinh@evan.team>
2019-12-13 12:44:44 +0100

    add evan-table component
    - [CORE-746]

---
+++ dapps/evancore.vue.libs/src/components/table/table.ts
@@ -0,0 +1,35 @@
+/*
+  Copyright (C) 2018-present evan GmbH.
+
+  This program is free software: you can redistribute it and/or modify it
+  under the terms of the GNU Affero General Public License, version 3,
+  as published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program. If not, see http://www.gnu.org/licenses/ or
+  write to the Free Software Foundation, Inc., 51 Franklin Street,
+  Fifth Floor, Boston, MA, 02110-1301 USA, or download the license from
+  the following URL: https://evan.network/license/
+*/
+
+// vue imports
+import Component, { mixins } from 'vue-class-component';
+import { Prop } from 'vue-property-decorator';
+
+// evan.network imports
+import EvanComponent from '../../component';
+
+/**
+ * Data Table based on bootstrap table
+ * https://bootstrap-vue.js.org/docs/components/table
+ *
+ * @class         SuccessComponent
+ * @selector      evan-success
+ */
+@Component({})
+export default class EvanTable extends mixins(EvanComponent) {}
